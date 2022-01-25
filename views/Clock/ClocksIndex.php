d3a434a9816d64fe0d4f3ff3d0ad64e732364cb8
sshkrlv <sashakorfly@gmail.com>
2022-01-25 08:08:12 +0300

    styles moved

--- views/Clock/ClocksIndex.php
+++ views/Clock/ClocksIndex.php
@@ -61,11 +61,7 @@
 <?php endif;?>
 
 
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mapModal" style="
-    margin: 8px;
-    bottom: 8px;
-    left: 90vw;
-    position: sticky;">
+<button type="button" class="btn btn-primary btn-map" data-bs-toggle="modal" data-bs-target="#mapModal">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
         <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
     </svg>
